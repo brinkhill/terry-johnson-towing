68d5a8f735779dd1d41afb73b97a3a45678b0f22
Hill, Brinkley Scott (IT System Verification and Testing) <bhill100@liberty.edu>
2024-11-06 11:17:26 -0500

    HUGE PUSH

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
+export default {
+ content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+ theme: {
+  extend: {
+   fontFamily: {
+    sans: ["Plus Jakarta Sans Variable", ...defaultTheme.fontFamily.sans],
+    serif: ["Space Grotesk", ...defaultTheme.fontFamily.serif],
+   },
+
+   backgroundImage: {
+    truck: 'url("/truck.jpg")',
+   },
+  },
+ },
+ plugins: [],
+};
